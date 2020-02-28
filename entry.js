dc0dce2238225c2622e78065371f2387a8256496
chong12 <chong12@myseneca.ca>
2020-02-27 22:04:49 -0500

    Validation for forms and email code

--- entry.js
+++ entry.js
@@ -12,12 +12,15 @@ const sections = require("./model/categories");
 
 const best_sellers = require("./model/bestseller");
 
+//load environment variables for keys
+require('dotenv').config({path:"./config/keys.env"});
+
 app.engine("handlebars", exphbs());
 app.set("view engine", "handlebars");
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static("public"));
 
-const PORT = process.env.PORT || 5500;
+const PORT = process.env.PORT;
 
 
 app.get("/", (req, res) => {
@@ -65,24 +68,164 @@ app.get("/login", (req, res) => {
 
 app.post("/registration", (req, res) => {
 
-    res.render("home", {
-        title: "Home",
-        header: "Home",
-        category: sections.getData(),
-        bestsellers: data.getData()
-    });
+    const { registration_id, email, password, c_password } = req.body;
+
+    let error1 = "";
+
+    let error2 = "";
+
+    let error3 = [];
+
+    let error4 = "";
+
+    let num_errors = 0;
+
+    if (registration_id == "") {
+
+        num_errors++;
+
+        error1 += "Please enter a UserName";
+
+    }
+
+    if (email == "") {
+
+        num_errors++;
+
+        error2 += "Please enter an email";
+
+    }
+
+    if (password.match(/^[A-Za-z0-9]+$/) == null) {
+
+        num_errors++;
+
+        error3.push("Password should only contain numbers and letters");
+
+    }
+
+    if (password.length < 6) {
+
+        num_errors++;
+
+
+        error3.push("Password should should be at least 6 characters long");
+
+    }
+
+    if (c_password != password) {
+
+        num_errors++;
+
+        error4 += "Passwords must match";
+
+    }
+
+    if (num_errors > 0) {
+
+        res.render("registration", {
+            title: "Registration",
+            header: "Registration",
+            registration_id: req.body.registration_id,
+            email: req.body.email,
+            password: req.body.password,
+            c_password: req.body.c_password,
+            error1: error1,
+            error2: error2,
+            error3: error3,
+            error4: error4,
+
+        });
+
+
+    } else {
+
+
+        const sgMail = require('@sendgrid/mail');
+        sgMail.setApiKey(process.env.SEND_GRID_API_KEY);
+        const msg = {
+            to: `${email}`,
+            from: `chong12@myseneca.ca`,
+            subject: 'Web322 Assignment',
+            html: `Welcome ${registration_id}. Registration was Successful`,
+        };
+
+
+        sgMail.send(msg)
+        .then(()=>{
+
+            res.render("home", {
+                title: "Home",
+                header: "Home",
+                category: sections.getData(),
+                bestsellers: data.getData()
+            });
+        })
+        .catch(err=>{
+
+            console.log(`Error ${{err}}`)
+
+        })
+
+
+
+    }
+
+
 
 
 });
 
 app.post("/login", (req, res) => {
 
-    res.render("home", {
-        title: "Home",
-        header: "Home",
-        category: sections.getData(),
-        bestsellers: data.getData()
-    });
+    const { l_user_id, password } = req.body;
+
+    let error1 = "";
+
+    let error2 = "";
+
+    let num_errors = 0;
+
+    if (l_user_id == "") {
+
+        num_errors++;
+
+        error1 = "Please enter a username";
+
+    }
+
+    if (password == "") {
+
+        num_errors++;
+
+        error2 = "Please enter a password";
+
+    }
+
+    if (num_errors > 0) {
+
+        res.render("login", {
+            title: "Login",
+            header: "Login",
+            l_user_id: req.body.l_user_id,
+            password: req.body.password,
+            msg_1: error1,
+            msg_2: error2
+        });
+
+
+    } else {
+
+        res.render("home", {
+            title: "Home",
+            header: "Home",
+            category: sections.getData(),
+            bestsellers: data.getData()
+        });
+
+    }
+
+
 
 
 });
